02544b416e9f8ac9e5d4bfa25f59d876fe292975
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-02 08:08:36 +0500

    Setup env

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
 
 const app: Express = express();
-const port = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
