18eb4c7c50548133efb877abdc6ab9dced4d98e7
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-02 07:28:14 +0500

    installed prisma and configured types for node project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 
-const app = express();
+const app: Express = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
